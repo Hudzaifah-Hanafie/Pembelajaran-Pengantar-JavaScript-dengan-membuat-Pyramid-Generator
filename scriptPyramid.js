88b241aea290883ef3562fa1f3c9b2e94bbcf293
Hudzaifah-Hanafie <hanafiyaahmad@gmail.com>
2025-07-25 14:23:43 +0700

    mempelajari penggunaan function dan sifatnya

--- scriptPyramid.js
+++ scriptPyramid.js
@@ -2,10 +2,23 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+function padRow(name) {
+  const test = "Testing";
+  console.log("This works!");
+  return test;
+  console.log("This works!");
+}
+
+const call = padRow("H");
+console.log(call);
+
 for (let i = 0; i < count; i++) {
-    rows.push(i);
-    
+  rows.push(character.repeat(i + 1));
 }
 
 let result = "";
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
 console.log(result);
