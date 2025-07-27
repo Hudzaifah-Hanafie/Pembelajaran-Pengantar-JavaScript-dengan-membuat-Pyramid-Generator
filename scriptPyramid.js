4271ca9c79fa7d4650293503e3c7bc97c54cf439
Hudzaifah-Hanafie <hanafiyaahmad@gmail.com>
2025-07-27 21:19:47 +0700

    merapihkan hasil testing penggunaan function

--- scriptPyramid.js
+++ scriptPyramid.js
@@ -2,14 +2,11 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(name) {
+function padRow() {
   const test = "Testing";
-  console.log("This works!");
   return test;
-  console.log("This works!");
 }
-
-const call = padRow("H");
+const call = padRow();
 console.log(call);
 
 for (let i = 0; i < count; i++) {
