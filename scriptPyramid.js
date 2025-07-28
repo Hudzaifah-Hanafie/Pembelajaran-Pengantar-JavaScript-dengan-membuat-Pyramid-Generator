1afb51b8bdc744d473f66e14381049180602de66
Hudzaifah-Hanafie <hanafiyaahmad@gmail.com>
2025-07-28 11:06:00 +0700

    melakukan pembelajaran pada penggunaan dan isi function padRow, operator, concetation, shorthand, dan pengantar penggunaan statement if

--- scriptPyramid.js
+++ scriptPyramid.js
@@ -2,15 +2,17 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow() {
-  const test = "Testing";
-  return test;
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-const call = padRow();
-console.log(call);
 
-for (let i = 0; i < count; i++) {
-  rows.push(character.repeat(i + 1));
+// TODO: use a different type of loop
+// for (let i = 1; i <= count; i++) {
+//   rows.push(padRow(i, count));
+// }
+
+if ("false") {
+  console.log("Condition is true")
 }
 
 let result = "";
